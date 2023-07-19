673c049fc583a1eb0ab1ec511e98bc19527cf75a
satvrn <pastawho@gmail.com>
2023-07-19 00:06:53 +0000

    prep to publish

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+//! A cute and smol RNG library.
+//!
+//! ## Features
+//! - Zero dependencies (by default).
+//! - `no_std` and WASM compatible.
+//! - No alloc usage.
+//! - 100% safe.
+//!
+//! ## Optional Features
+//! - `system-rng` adds support for using system RNG, which uses the [`getrandom`](https://crates.io/crates/getrandom) crate.
+//! - `zeroize` adds zeroing support to all RNGs, which uses the [`zeroize`](https://crates.io/crates/zeroize) crate.
+//!
+//! ## **M**inimum **S**upported **R**ust **V**ersion (MSRV)
+//!
+//! The current MSRV is 1.60.0.
+
 #![no_std]
 #![forbid(unsafe_code)]
 
